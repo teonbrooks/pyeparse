aa542e39d35b2cd823dd777bc494987f65e13ca7
teonlamont <teon.brooks@gmail.com>
2015-05-17 16:41:06 +0200

    ENH: Add functioning InterestAreas

--- pyeparse/ia.py
+++ pyeparse/ia.py
@@ -3,21 +3,15 @@
 #
 # License: BSD (3-clause)
 
-from copy import deepcopy
+from copy import copy, deepcopy
 import numpy as np
-from scipy.optimize import fmin_slsqp
-import warnings
 from collections import OrderedDict
 
-from ._event import Discrete
-from .viz import plot_epochs
-from .utils import pupil_kernel
-from ._fixes import string_types, nanmean, nanstd
-from .parallel import parallel_func
+from ._fixes import string_types
 from ._baseraw import read_raw, _BaseRaw
 
 
-class InterestAreas(OrderedDict):
+class InterestAreas(object):
     """ Create interest area summaries for Raw
 
     Parameters
@@ -35,10 +29,8 @@ class InterestAreas(OrderedDict):
     event_id : int | dict
         The event ID to use. Can be a dict to supply multiple event types
         by name.
-    dep : str
+    depmeas : str
         Dependent measure. 'fix' for fixations, 'sac' for saccades.
-    ignore_missing : bool
-        If True, do not warn if no events were found.
 
     Returns
     -------
@@ -46,10 +38,13 @@ class InterestAreas(OrderedDict):
         The epoched dataset.
         Trial x IAS x fixations/saccades
     """
-    def __init__(self, raw, ias, event_id=42, dep='fix'):
-        if isinstance(raw, str):
+    def __init__(self, raw, ias, event_id=42, depmeas='fix'):
+        if isinstance(raw, string_types):
             raw = read_raw(raw)
-        assert isinstance(raw, _BaseRaw)
+        if not isinstance(raw, _BaseRaw):
+            raise TypeError('raw must be Raw instance of filename, not %s'
+                            % type(raw))
+
         trial_ids = raw.find_events('TRIALID', 1)
         self.n_trials = n_trials = trial_ids.shape[0]
         self.n_ias = n_ias = ias.shape[0]
@@ -58,50 +53,97 @@ class InterestAreas(OrderedDict):
         msg = last[-1].split()
         last[-1] = ' '.join([msg[0], str(int(msg[-1]) + 1)])
         trial_ids = np.vstack((trial_ids, last))
-        _trial_starts = [int(trial_ids[i][0]) for i, _ in enumerate(trial_ids[:-1])]
-        _trial_ends = [int(trial_ids[i+1][0]) for i, _ in enumerate(trial_ids[:-1])]
-        _trials = zip(_trial_starts, _trial_ends)
-        _trial_durations = [end - start for start, end in _trials]
-        
-        ias_ = [[list() for ia in range(n_ias)] for _ in range(n_trials)]
+        t_starts = [int(trial_ids[i][0]) for i, _ in enumerate(trial_ids[:-1])]
+        t_ends = [int(trial_ids[i+1][0]) for i, _ in enumerate(trial_ids[:-1])]
+        self._trials = trials = zip(t_starts, t_ends)
+        self.trial_durations = [end - start for start, end in trials]
 
+        ias_ = [[list() for ia in range(n_ias)] for _ in range(n_trials)]
         fix_order = [list() for _ in range(n_trials)]
-        for fix in raw.discrete['fixations']:
-            eye, stime, etime, x_pos, y_pos = fix
-            for jj, trial in enumerate(_trials):
-                tstart, tend = trial
-                if tstart < stime * 1000 < tend:
-                    fix_order[jj].append(int(stime * 1000))                                
-                    # RECTANGLE id left top right bottom [label]
-                    for ii, ia in enumerate(ias):
-                        _, _, ia_left, ia_top, ia_right, ia_bottom, _ = ia
-                        if int(ia_left) < int(x_pos) < int(ia_right) \
-                            and int(ia_top) < int(y_pos) < int(ia_bottom):
-                            ias_[jj][ii].append(list(fix))
+
+        if depmeas == 'fix':
+            depmeas = raw.discrete['fixations']
+            labels = ['eye', 'stime', 'etime', 'axp', 'ayp']
+            n_meas = len(labels)
+            for meas in depmeas:
+                eye, stime, etime, xp, yp = meas
+                meas = np.array(meas)
+                for jj, trial in enumerate(trials):
+                    tstart, tend = trial
+                    if tstart < stime * 1000 < tend:
+                        fix_order[jj].append(int(stime * 1000))                                
+                        # RECTANGLE id left top right bottom [label]
+                        for ii, ia in enumerate(ias):
+                            _, _, ia_left, ia_top, ia_right, ia_bottom, _ = ia
+                            if int(ia_left) < int(xp) < int(ia_right) \
+                                and int(ia_top) < int(yp) < int(ia_bottom):
+                                ias_[jj][ii] = meas
+            for jj in range(n_trials):
+                for ii in range(n_ias):
+                    if isinstance(ias_[jj][ii], list):
+                        ias_[jj][ii] = np.rec.fromarrays(np.ones(n_meas) *
+                                                         np.nan, names=labels)
+        elif depmeas == 'sac':
+            depmeas = raw.discrete['saccades']
+            labels = ['eye', 'stime', 'etime', 'sxp', 'syp',
+                      'exp', 'eyp', 'pv']
+            n_meas = len(labels)
+            for meas in depmeas:
+                eye, stime, etime, sxp, syp, exp, eyp, pv = meas
+                meas = np.array(meas)
+                for jj, trial in enumerate(trials):
+                    tstart, tend = trial
+                    if tstart < stime * 1000 < tend:
+                        fix_order[jj].append(int(stime * 1000))                                
+                        # RECTANGLE id left top right bottom [label]
+                        for ii, ia in enumerate(ias):
+                            _, _, ia_left, ia_top, ia_right, ia_bottom, _ = ia
+                            if int(ia_left) < int(sxp) < int(ia_right) \
+                                and int(ia_top) < int(syp) < int(ia_bottom):
+                                ias_[jj][ii] = meas
+            for jj in range(n_trials):
+                for ii in range(n_ias):
+                    if isinstance(ias_[jj][ii], list):
+                        ias_[jj][ii] = np.rec.fromarrays(np.ones(n_meas) *
+                                                         np.nan, names=labels)
 
         ia_labels = list(ias[:, -1])
         # ordered dict
-        temp = [dict(zip(ia_labels, [np.array(ia) for ia in trial])) for trial in ias_]
-        super.__init__(zip(range(n_trials), temp))
+        temp = [IA(zip(ia_labels, trial)) for trial in ias_]
+        self._data = temp
 
     def __repr__(self):
-        return '<IA | {0} Trials x {0} IAs>'.format(self.n_trials, self.n_ias)
+        return '<IA | {0} Trials x {1} IAs>'.format(self.n_trials, self.n_ias)
+
+    def __len__(self):
+        return len(self._data)
+
+    @property
+    def shape(self):
+        return (self.n_trials, self.n_ias)
 
     def __getitem__(self, idx):
         if isinstance(idx, string_types):
-            if not idx in self[0]:
+            if not idx in self._data[0]:
                 raise KeyError("'%s' is not found." % idx)
-            return [d[idx] for d in self.values()]
+            return np.hstack([datum[idx] for datum in self._data])
         elif isinstance(idx, int):
-            return self.values()[idx]
+            return self._data[idx]
         elif isinstance(idx, slice):
-            return self.values()[idx]
+            inst = copy(self)
+            inst._data = self._data[idx]
+            inst.n_trials = len(inst._data)
+            return inst
         else:
             raise TypeError('index must be an int, string, or slice')
 
-# Look for trial start and stop
-# Gather all the data for the trial
-# First fixation, then work on saccade
+
+class IA(OrderedDict):
+    def __init__(self, entry):
+        super(IA, self).__init__(entry)
+
+    def __repr__(self):
+        return '<Interest Areas: %s>' % ', '.join(self.keys())
 
 
 def read_ia(filename):
